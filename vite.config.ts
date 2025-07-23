1f448e2ba752df2334229ec439468234a59b76fa
Alain Perkaz <alain.perkaz@fortnox.se>
2025-07-23 16:39:48 +0200

    Example with real fetch and manual runtime validation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // This proxies requests starting with /pet/
+      "/pet": {
+        target: "https://petstore3.swagger.io/api/v3/pet",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/pet/, ""),
+      },
+    },
+  },
+});
